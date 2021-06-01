5194f4ad63dee721950fddd850f49021ce00cdff
Ather <mailtoather17@gmail.com>
2021-06-01 12:03:56 +0530

    Initial Commit

---
+++ spotify/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  spotify
+//
+//  Created by Ather Hussain on 01/06/21.
+//
+
+import UIKit
+
+class HomeViewContorller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
