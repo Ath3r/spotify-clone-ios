7d8fefd9c8b738adb3c0c0aca6d40b22c3571d68
Ather <mailtoather17@gmail.com>
2021-06-15 18:30:11 +0530

    Profile Setup

--- spotify/Controllers/Others/ProfileViewController.swift
+++ spotify/Controllers/Others/ProfileViewController.swift
@@ -7,21 +7,76 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.isHidden = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tableView
+    }()
+    
+    private var models = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
-        APICaller.shared.getUserProfile { result in
-            switch result{
-            case .success(let model):
-                break
-            case .failure(let error):
-                print(error.localizedDescription)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        fetchProfile()
+        view.backgroundColor = .systemBackground
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func fetchProfile(){
+        APICaller.shared.getUserProfile {[weak self] result in
+            DispatchQueue.main.async {
+                switch result{
+                case .success(let model):
+                    self?.updateUI(with: model)
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self?.failedToGetProfile()
+                }
             }
         }
     }
     
-
+    private func updateUI(with model: UserProfile){
+        tableView.isHidden = false
+        
+        models.append("Full Name: \(model.display_name)")
+        models.append("Email Address: \(model.email)")
+        models.append("User ID: \(model.id)")
+        models.append("Plan: \(model.product)")
+        tableView.reloadData()
+    }
+    
+    private func failedToGetProfile(){
+        let label = UILabel(frame: .zero)
+        label.text = "Failed to get Profile"
+        label.sizeToFit()
+        label.textColor = .secondaryLabel
+        view.addSubview(label)
+        label.center = view.center
+    }
+    
+    //MARK: - TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
 
 }
