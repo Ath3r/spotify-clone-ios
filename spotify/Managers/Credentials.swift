5dbe2c9236652c9e1506cf832b6264bd10ac237f
Ather <mailtoather17@gmail.com>
2021-06-18 22:05:04 +0530

    gitignore file added

---
+++ spotify/Managers/Credentials.swift
@@ -0,0 +1,14 @@
+//
+//  Credentials.swift
+//  spotify
+//
+//  Created by Ather Hussain on 18/06/21.
+//
+
+import Foundation
+
+class Spotify {
+    static let clientId = ""
+    static let clientSecret = ""
+    static let websiteURL = ""
+}
